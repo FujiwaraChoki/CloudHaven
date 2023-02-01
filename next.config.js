ea76bd47bebf26b9114cbb129f2720a756079e44
Sami Hindi <lolipop66611@gmail.com>
2023-02-01 16:15:31 +0100

    Replace

--- next.config.js
+++ next.config.js
@@ -1,42 +1,6 @@
-module.exports = {
-    images: {
-      remotePatterns: [
-        {
-          protocol: "https",
-          hostname: "ae01.alicdn.com",
-          port: "",
-        },
-        {
-          protocol: "https",
-          hostname: "cdn.shopify.com",
-          port: "",
-        },
-      ],
-    },
-    async headers() {
-      return [
-        {
-          source: "/api/:path*",
-          headers: [
-            {
-              key: "Access-Control-Allow-Credentials",
-              value: "true",
-            },
-            {
-              key: "Access-Control-Allow-Origin",
-              value: "*",
-            },
-            {
-              key: "Access-Control-Allow-Methods",
-              value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
-            },
-            {
-              key: "Access-Control-Allow-Headers",
-              value:
-                "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
-            },
-          ],
-        },
-      ];
-    }
-  };
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+}
+
+module.exports = nextConfig
