040518e57ef0bfb78722df86c463ff2af1153ac3
Sami Hindi <lolipop66611@gmail.com>
2023-02-01 15:52:04 +0100

    Added Next.js to CloudHaven

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Head from "next/Head";
+
+export default function Home() {
+    return (
+        <h1>
+            <Head>
+                <title>CloudHaven</title>
+            </Head>
+            CloudHaven
+        </h1>
+    )
+}
