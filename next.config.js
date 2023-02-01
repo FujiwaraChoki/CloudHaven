7caeb3d4c39a4c8b2fd1f94ae784a15ab39665f0
Sami Hindi <lolipop66611@gmail.com>
2023-02-01 15:55:10 +0100

    Updated next.config.js file

--- next.config.js
+++ next.config.js
@@ -1,10 +1,42 @@
 module.exports = {
-    webpack: (config) => {
-      // Fixes npm packages that depend on `fs` module
-      config.node = {
-        fs: 'empty'
-      }
-  
-      return config
+    images: {
+      remotePatterns: [
+        {
+          protocol: "https",
+          hostname: "ae01.alicdn.com",
+          port: "",
+        },
+        {
+          protocol: "https",
+          hostname: "cdn.shopify.com",
+          port: "",
+        },
+      ],
+    },
+    async headers() {
+      return [
+        {
+          source: "/api/:path*",
+          headers: [
+            {
+              key: "Access-Control-Allow-Credentials",
+              value: "true",
+            },
+            {
+              key: "Access-Control-Allow-Origin",
+              value: "*",
+            },
+            {
+              key: "Access-Control-Allow-Methods",
+              value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+            },
+            {
+              key: "Access-Control-Allow-Headers",
+              value:
+                "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+            },
+          ],
+        },
+      ];
     }
-  }
+  };
