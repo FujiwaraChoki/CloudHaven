f9e2a887808535741955a427e86f01a6903eabf3
Sami Hindi <lolipop66611@gmail.com>
2023-02-02 09:17:06 +0100

    Updating

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+module.exports = {
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+const path = require("path");
+
+module.exports = {
+  distDir: "build",
+  sassOptions: {
+      includePaths: [path.join(__dirname, "styles")],
+  },
+};
