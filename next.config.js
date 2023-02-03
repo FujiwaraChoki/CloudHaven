df31413ddbb47e2d03d5462c36767d0e2a6b563e
Sami Hindi <lolipop66611@gmail.com>
2023-02-03 11:47:04 +0100

    Cap

--- next.config.js
+++ next.config.js
@@ -1,12 +1,3 @@
 module.exports = {
-  reactStrictMode: true,
-};
-
-const path = require("path");
-
-module.exports = {
   distDir: "build",
-  sassOptions: {
-      includePaths: [path.join(__dirname, "styles")],
-  },
 };
