a725f1885fed01b5ee4bde143b9e9dfaf5f3fdd9
Sami Hindi <lolipop66611@gmail.com>
2023-02-17 16:49:47 +0100

    Config File Next.js updated

--- next.config.js
+++ next.config.js
@@ -1,14 +1,7 @@
-const path = require("path");
-
 module.exports = {
     images: {
         domains: ["localhost"],
         unoptimized: true,
     },
     distDir: "build",
-    webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-        config.resolve.alias["components"] = path.join(__dirname, "components");
-        config.resolve.alias["styles"] = path.join(__dirname, "styles");
-        return config;
-    }
 }
